48690fd43c1ea2078a0df25272b8827000a17f2b
Jorrit Oosterhof <jorrito@icloud.com>
2015-11-28 17:03:38 +0100

    Adding Xcode project for the framework

---
+++ ZeeguuAPI/ZeeguuAPI/ZeeguuAPI.swift
@@ -0,0 +1,14 @@
+//
+//  ZeeguuAPI.swift
+//  ZeeguuAPI
+//
+//  Created by Jorrit Oosterhof on 28-11-15.
+//  Copyright © 2015 Jorrit Oosterhof. All rights reserved.
+//
+
+import UIKit
+/// This object can be used to connect to the Zeeguu API.
+public class ZeeguuAPI: NSObject {
+	
+	
+}
