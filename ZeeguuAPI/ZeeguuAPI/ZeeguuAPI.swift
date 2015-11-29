191a2c55c474d62430b19144ee702123c0623cf2
Jorrit Oosterhof <jorrito@icloud.com>
2015-11-29 11:37:35 +0100

    Added first part of API object

--- ZeeguuAPI/ZeeguuAPI/ZeeguuAPI.swift
+++ ZeeguuAPI/ZeeguuAPI/ZeeguuAPI.swift
@@ -7,8 +7,34 @@
 //
 
 import UIKit
+
 /// This object can be used to connect to the Zeeguu API.
-public class ZeeguuAPI: NSObject {
+public class ZeeguuAPI {
+	private static let instance = ZeeguuAPI()
 	
+	private var currentSessionID: Int
 	
+	/// Returns if a user is logged in currently
+	public var isLoggedIn: Bool {
+		get {
+			return currentSessionID != 0
+		}
+	}
+	
+	/// Get the ZeeguuAPI instance
+	public static func sharedAPI() -> ZeeguuAPI {
+		return instance;
+	}
+	
+	private init() {
+		self.currentSessionID = 0
+		let def = NSUserDefaults.standardUserDefaults()
+		if (def.objectForKey(ZeeguuAPI.sessionIDKey) != nil) {
+			self.currentSessionID = def.objectForKey(ZeeguuAPI.sessionIDKey)!.integerValue
+		}
+	}
+	
+	public func addUserWithUserName(userName: String, email: String, password: String) {
+		
+	}
 }
---
+++ ZeeguuAPI/ZeeguuAPI/ZeeguuAPIDefaults.swift
@@ -0,0 +1,13 @@
+//
+//  ZeeguuDefaults.swift
+//  ZeeguuAPI
+//
+//  Created by Jorrit Oosterhof on 28-11-15.
+//  Copyright © 2015 Jorrit Oosterhof. All rights reserved.
+//
+
+import UIKit
+
+extension ZeeguuAPI {
+	static let sessionIDKey: String = "ZeeguuSessionID"
+}
