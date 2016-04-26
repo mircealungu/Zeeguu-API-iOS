633f151ae964db0e65acaabbeab62a7903730902
Jorrit Oosterhof <jorrito@icloud.com>
2016-04-26 16:48:35 +0200

    Changed the way contents and difficulties are retrieved and added some convenience methods to Article

--- ZeeguuAPI/ZeeguuAPITests/ZeeguuAPITests.swift
+++ ZeeguuAPI/ZeeguuAPITests/ZeeguuAPITests.swift
@@ -401,9 +401,9 @@ class ZeeguuAPITests: XCTestCase {
 	
 	func testGetDifficultyForTexts() {
 		print("Testing get difficulty for texts:")
-		ZeeguuAPI.sharedAPI().getDifficultyForTexts([ZeeguuAPITests.TEST_TEXT], langCode: "de") { (dict) -> Void in
-			XCTAssertNotNil(dict)
-			print("dict: ", dict)
+		ZeeguuAPI.sharedAPI().getDifficultyForTexts([ZeeguuAPITests.TEST_TEXT], langCode: "de") { (difficulties) in
+			XCTAssertNotNil(difficulties)
+			print("difficulties: ", difficulties)
 			self.testLock.signal()
 		}
 		testLock.wait()
@@ -421,9 +421,9 @@ class ZeeguuAPITests: XCTestCase {
 	
 	func testGetContentFromURLs() {
 		print("Testing get content from urls:")
-		ZeeguuAPI.sharedAPI().getContentFromURLs(["http://www.t-online.de/nachrichten/deutschland/id_76314572/frank-juergen-weise-geraet-wegen-langer-asylverfahren-in-die-kritik.html", "http://www.derbund.ch/wirtschaft/unternehmen-und-konjunktur/die-bankenriesen-in-den-bergkantonen/story/26984250", "http://www.computerbase.de/2015-11/bundestag-parlament-beschliesst-das-ende-vom-routerzwang-erneut/", "http://www.spiegel.de/panorama/justiz/beate-zschaepe-im-nsu-prozess-was-sie-ausgesagt-hat-a-1066805.html"]) { (dict) -> Void in
-			XCTAssertNotNil(dict)
-			print("dict: ", dict)
+		ZeeguuAPI.sharedAPI().getContentFromURLs(["http://www.t-online.de/nachrichten/deutschland/id_76314572/frank-juergen-weise-geraet-wegen-langer-asylverfahren-in-die-kritik.html", "http://www.derbund.ch/wirtschaft/unternehmen-und-konjunktur/die-bankenriesen-in-den-bergkantonen/story/26984250", "http://www.computerbase.de/2015-11/bundestag-parlament-beschliesst-das-ende-vom-routerzwang-erneut/", "http://www.spiegel.de/panorama/justiz/beate-zschaepe-im-nsu-prozess-was-sie-ausgesagt-hat-a-1066805.html"]) { (contents) in
+			XCTAssertNotNil(contents)
+			print("contents: ", contents)
 			self.testLock.signal()
 		}
 		testLock.wait()
